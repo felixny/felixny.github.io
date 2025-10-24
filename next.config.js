1f7aeee79d3276541744deb8ec65d9aef3aca209
felixny <felixynx@gmail.com>
2025-10-24 18:54:37 +0000

    Fix Next.js config for static export and rebuild with LinkedIn/GitHub links

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    assetPrefix: '',
     images: {
         unoptimized: true,
         domains: ['images.unsplash.com'],
